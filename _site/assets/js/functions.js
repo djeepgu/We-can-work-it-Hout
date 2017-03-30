c57d2d202e8311a1a962b0806511fc9ae2371e12
djeepgu <djeepgu@gmail.com>
2017-03-30 12:51:43 +0200

    reorganising

--- _site/assets/js/functions.js
+++ _site/assets/js/functions.js
@@ -1,5 +1,13 @@
 $( document ).ready(function() {
+/*
+  $("div > b").on('click', function() {
 
-  // FUNCTIONS HERE
+    $('.header').css('height', '64px');
+    $('b').css('margin-top', '12px');
+    $('b').css('animation', 'none');
+    $('b:hover').css('cursor', 'auto');
+    $('.home-page-content').css('display', 'block');
+  });
+*/
 
 });
