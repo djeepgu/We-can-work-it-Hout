8f1b3c2c1c6d8280ca6803c903d08f036f2092d5
djeepgu <djeepgu@gmail.com>
2017-04-03 12:33:35 +0200

    adjustments

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,13 +1,33 @@
 $( document ).ready(function() {
-/*
-  $("div > b").on('click', function() {
 
-    $('.header').css('height', '64px');
-    $('b').css('margin-top', '12px');
-    $('b').css('animation', 'none');
-    $('b:hover').css('cursor', 'auto');
-    $('.home-page-content').css('display', 'block');
+  // AUTOMATIC COMPANY NAME SLIDE UP
+  setTimeout( function() {
+    // RESIZE HEIGHT OF HEADER
+    $("#home-page .header").css('height', '64px')
+
+  }, 3000);
+
+    // AFTER NAME SLIDES UP SHOW NAVIGATION BAR
+  setTimeout( function() {
+
+    $("#home-page .nav").css('visibility', 'visible')
+
+  }, 5000);
+
+    // NAVIGATION ACTIVE STATE
+      // TAKE INDEX OF THE ACTIVE MENU
+  //var activeMenu = $(".nav .active").index();
+      // CHECK WHICH MENU HAS BEEN CLICKED
+  $(".nav ul li").on("click", function() {
+    var clickedMenu = $(this).index();
+    // REMOVE active CLASS FROM activeMenu
+    $(this).removeClass("active");
+    // ADD active CLASS TO THE CLICKED MENU
+    $(":nth-child("+clickedMenu+")", this).addClass("active");
+    //alert(clickedMenu);
+    //alert(activeMenu);
+    //alert("test");
   });
-*/
+
 
 });
