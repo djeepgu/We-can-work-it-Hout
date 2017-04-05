c21be288b315111e3338d9a6a4aeeeec81da5f42
djeepgu <djeepgu@gmail.com>
2017-04-05 15:01:11 +0200

    prototype
    presentation version

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -3,8 +3,8 @@ $( document ).ready(function() {
   // AUTOMATIC COMPANY NAME SLIDE UP
   setTimeout( function() {
     // RESIZE HEIGHT OF HEADER
-    $("#home-page .header").css('height', '64px')
-
+    $("#home-page .header").css('height', '64px');
+    $("#home-page .stripes .drop-letter").css('height', '64px');
   }, 3000);
 
     // AFTER NAME SLIDES UP SHOW NAVIGATION BAR
@@ -27,6 +27,45 @@ $( document ).ready(function() {
     //alert(clickedMenu);
     //alert(activeMenu);
     //alert("test");
+  });
+
+
+  // SIDEBAR UP/DOWN NAVIGATION
+
+    //DOWN
+  $("#down").click(function () {
+    var count = $(".portfolio > div").length;
+    var activeView = $(".portfolio").find(".in-view").index() + 1;
+    var downNext = activeView + 1;
+
+    if (count == activeView) {
+      $(".portfolio").scrollTo("#pg1", 500);
+      $(".portfolio > div:nth-last-child(1)").removeClass("in-view");
+      $("#pg1").addClass("in-view");
+    } else {
+      $(".portfolio").scrollTo("#pg"+downNext+"", 500);
+      $(".portfolio > div:nth-child("+activeView+")").removeClass("in-view");
+      $(".portfolio > div:nth-child("+downNext+")").addClass("in-view");
+    }
+
+  });
+
+    //UP
+  $("#up").click(function () {
+    var count = $(".portfolio > div").length;
+    var activeView = $(".portfolio").find(".in-view").index() + 1;
+    var upNext = activeView - 1;
+
+    if (activeView == 1) {
+      $(".portfolio").scrollTo("#pg"+count+"", 500);
+      $("#pg1").removeClass("in-view");
+      $(".portfolio > div:nth-last-child(1)").addClass("in-view");
+    } else {
+      $(".portfolio").scrollTo("#pg"+upNext+"", 500);
+      $(".portfolio > div:nth-child("+activeView+")").removeClass("in-view");
+      $(".portfolio > div:nth-child("+upNext+")").addClass("in-view");
+    }
+
   });
 
 
